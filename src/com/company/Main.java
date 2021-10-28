e20b9749304996343c35213ce25ff494a23c71d5
DaniloVFreire <dvf@ic.ufal.br>
2021-10-27 21:24:46 -0300

    Adição de forma primitiva de armazenamento de funcionário com arraylist de string

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //ArrayList <String> users = new ArrayList<>() ;
-        //System.out.println(users);
+        ArrayList <String> funcionariosNome = new ArrayList<>() ;
+        ArrayList <String> funcionariosEndereco = new ArrayList<>() ;
+        ArrayList <String> funcionariosTipo = new ArrayList<>() ;
+        ArrayList <String> funcionarioscpf = new ArrayList<>();
+
         boolean run = true;
         while(run){
         System.out.println("foi");
-        System.out.print("Sistema folha de pagamento, para:\n Adcionar um emprego" +
+        System.out.print("Sistema folha de pagamento, para:\n" +
                 "Adicionar um empregado digite 1;\n" +
                 "Para remover um empregado digite 2;\n" +
                 "Para lançar um cartão de ponto digite 3;\n" +
@@ -24,14 +27,46 @@ public class Main {
                 "Para mudar agenda de pagamento digite 10;\n" +
                 "Para criar nova agenda de pagamento digite 11;\n" +
                 "para sair digite 12;\n");
+        System.out.println(funcionariosNome+ "\n");
+            System.out.println(funcionariosEndereco+ "\n");
+            System.out.println(funcionariosTipo+ "\n");
+            System.out.println(funcionarioscpf+ "\n");
         Scanner scanner = new Scanner(System.in);
 
-        int entrada = scanner.nextInt();
-        System.out.println(entrada);
+        int escolhaNumerica = scanner.nextInt();
+        System.out.println(escolhaNumerica);
 
-        switch (entrada) {
+        switch (escolhaNumerica) {
             case 1:
-                System.out.println("você escolhei a opção 1");
+                System.out.println("Digite o nome do funcionário:");
+                String nome = scanner.next();
+                funcionariosNome.add(nome);
+
+                System.out.print("Digite o endereço do funcionário " + nome + " :\n");
+                String endereco = scanner.next();
+                funcionariosEndereco.add(endereco);
+                while(true){
+                    System.out.print("Digite o tipo do funcionário " + nome + " :\n" +
+                            "Horista 1\n" +
+                            "Assalariado 2\n" +
+                            "Comissionado 3\n");
+                    int tipo = scanner.nextInt();
+                    if(tipo == 1){
+                        funcionariosTipo.add("hourly");
+                        break;
+                    }
+                    else if(tipo == 2){
+                        funcionariosTipo.add("salaried");
+                        break;
+                    }
+                    else if(tipo == 3){
+                        funcionariosTipo.add("commisioned");
+                        break;
+                    }
+
+                }
+
+
                 break;
             case 2:
 
